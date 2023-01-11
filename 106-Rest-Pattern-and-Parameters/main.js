2de5cd5a88e8952ace7dfd223ea95632d10b791c
Cter05 <ductruongtm03@gmail.com>
2023-01-11 09:24:42 +0700

    106_Rest_Pattern_and_Parameters

--- 106-Rest-Pattern-and-Parameters/main.js
+++ 106-Rest-Pattern-and-Parameters/main.js
@@ -29,36 +29,45 @@ const restaurant = {
     {// Ham
         console.log(`Order received! ${this.staterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     },
+
+    orderPasta: function (ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1},${ing2},${ing3}`);
+    },
+     orderPizza: function(mainIngredient, ...otherIngredients){
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+
+     }
 };
-// Truyền một đối tượng vào hàm OrderDelivery
-restaurant.orderDelivery({
-    time: '22:30',
-    address: 'Via del Sole, 21',
-    mainIndex: 2,
-    starterIndex: 2,
-})
 
-restaurant.orderDelivery({
+// bởi vì ở phía bên phải của
+const arr = [1, 2, ...[3, 4]];
 
-    address: 'Via del Sole, 21',
-    starterIndex: 1,
-})
+const [a, b, ...orthers] = [1, 2, 3, 4, 5]
+console.log(a, b, orthers);
 
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+const [pizza, , risotto, ...ortherFood] = [...restaurant.mainMenu, ...restaurant.staterMenu];
+console.log(pizza, risotto, ortherFood);
 
-const { name: restaurantName, openingHours: hours, categories: tags, } = restaurant
-console.log(restaurantName, hours, tags);
-// gia tri mac dinh
-const { nmenu = [], staterMenu: starters = [] } = restaurant;
-console.log(nmenu, starters)
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-//Ghi de gia tri a va b , dung phep huy cau truc bang dau ngoac don
-({ a, b } = obj);
-console.log(a, b);
-// Doi tuong long nhau
-const { fri: { open, close } } = openingHours;
-console.log(open, close);
+// Object
+const { sat, ...weekday } = restaurant.openingHours;
+console.log(weekday);
+
+// 2) functions
+const add = function (...numbers) {
+    // console.log(numbers)
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    console.log(sum);
+}
+    add(2, 3),
+    add(5, 3, 7, 2),
+    add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23,5,7];
+add(...x)    
+
+restaurant.orderPizza('mushrooms','Onion','olives','spinach');
+restaurant.orderPizza('mushrooms')
