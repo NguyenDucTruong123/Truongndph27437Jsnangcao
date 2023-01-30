91694337454d683b3dc19ac1853051852b821a22
Cter05 <ductruongtm03@gmail.com>
2023-01-30 11:01:18 +0700

    114_Looping objects ....

---
+++ 114_Looping_Objects_Object_Keys_values_and_Entries/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const weekdays = ['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours= {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    // Cũng có thể làm cách này
+    // [`day-${2+4}`]: {
+    //     open: 0, // Open 24 hours
+    //     close: 24,
+    // },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    //ES6 enhanced object literals
+    openingHours,
+
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+// array keys
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open in ${properties.length} days: `;
+for (const day of properties) {
+    openStr += `${day},`
+}
+console.log(openStr);
+
+//Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+// entire objects
+const entires = Object.entries(openingHours);
+console.log(entires);
+// [key, value]
+for (const [key,{open, close}] of entires) {
+    console.log(`On ${key} we open at ${open} and close at ${close}`)
+}
